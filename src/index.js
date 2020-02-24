63d7668b7cfe2d6014518cd686cddad719358561
Rossevv <ros22ch.caceres@gmail.com>
2020-02-23 22:42:07 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,78 @@
+
+
 import validator from './validator.js';
+
+document.getElementById('expirationDate').addEventListener('keyup', (e) => {
+        let expiDate = e.target.value;
+document.getElementById('expirationDate').value = expiDate
+        .replace(/([0-9]{3})/g,'$1 ')
+        .replace(/\s/g, '/')
+});
+
+
+
+let inputNumber = document.getElementById('cardNumber');
+    inputNumber.addEventListener('keyup',(e) =>{
+    let prueba = e.target.value;
+    inputNumber.value = prueba.replace(/\D/g, '');
+    });
+let finalCondition = document.getElementById('conditionNum');
+
+let hideNumber = document.getElementById('hideNum');
+
+    document.getElementById('validate').addEventListener('click', (event) => {
+    event.preventDefault();
+    validator.isValid(inputNumber.value);
+    finalCondition.innerHTML = validator.isValid(inputNumber.value);
+    validator.maskify(inputNumber.value)
+    hideNumber.innerHTML = validator.maskify(inputNumber.value);
+    // console.log(validator.maskify('4083952015263'))
+    let numPos = document.querySelector('.numPos')
+    if(validator.isValid(inputNumber.value)==='Tarjeta inválida') {
+        numPos.classList.add('br');
+            }else{
+        numPos.classList.add('by');
+    }
+
+});
+
+document.getElementById('addOther').addEventListener('click', () => {
+
+    let inputs = document.getElementsByTagName("input");
+    //console.log(inputs)
+    for(var i=0;i<inputs.length;i++){
+        inputs[i].value = "";
+    }
+        finalCondition.innerHTML= "";
+        hideNumber.innerHTML = "";
+        let numPos = document.querySelector('.numPos')
+            if(numPos.classList.remove('br')) {
+                numPos.classList.remove('br');
+            }else{
+                numPos.classList.remove('by');
+            }
+
+})
+
+document.getElementById('finalizar').addEventListener('click', () => {
+    let inputs = document.getElementsByTagName("input");
+    //console.log(inputs)
+    for(var i=0;i<inputs.length;i++){
+        inputs[i].value = "";
+    }
+        finalCondition.innerHTML= "";
+        hideNumber.innerHTML = "";
+        let numPos = document.querySelector('.numPos')
+            if(numPos.classList.remove('br')) {
+                numPos.classList.remove('br');
+            }else{
+                numPos.classList.remove('by');
+            }
+})
+
+
+
+
+
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
 const validator = {
-  // ...
-};
+  isValid: (cardNumber) => {
+    let totalSum = 0;
+    let finalResult = true;
+    for (let i = cardNumber.length-1; i >=0; i--){
+      //console.log(i);
+      const integerValue = parseInt(cardNumber[i]);
+      //console.log(cardNumber[i]);
+      if (i % 2 !== 0){
+          const oddPosition = integerValue*2;
+          if(oddPosition >= 10){
+            let maxDiez = 1 + (oddPosition%10);
+            totalSum  = totalSum + maxDiez;
+          }else{
+            totalSum = totalSum + oddPosition;
+          }
+      }else{
+        totalSum = totalSum + integerValue;
+      }
+    }
+    if (totalSum % 10 === 0){
+      finalResult = true;
+    } else {
+      finalResult = false;
+    }
+    return (finalResult);
+},
 
+  maskify: (cardNumber) => {
+    let dividedNum = cardNumber.split('');
+    for (let i = 0; i < cardNumber.length-4; i++){
+      dividedNum[i] = '#';
+    }
+    return dividedNum.join('')
+}
+}
 export default validator;
+
+
