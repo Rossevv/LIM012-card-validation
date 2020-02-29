fd81e7e3724467abe9b71c4642c55aa8816e6270
Rossevv <ros22ch.caceres@gmail.com>
2020-02-28 19:12:06 -0500

    arreglo de variables

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import validator from './validator.js';
 console.log('./validator.js');
 /*Delcarando variables */
 
-let inputExpirationDate = document.getElementById('expirationDate');
-let inputNumber = document.getElementById('cardNumber');
-let finalConditionCardValidOrInvalid = document.getElementById('conditionNum');
-let cardNumberWithMaskify = document.getElementById('hideNum');
-let buttonValidateCardNumber = document.getElementById('validate');
-let buttonaAdOthercard = document.getElementById('addOther');
-let inputFinalProcess = document.getElementById('finalizar');
+let inputExpirationDate = document.getElementById('inputExpirationDate');
+let inputCardNumber = document.getElementById('inputCardNumber');
+let finalConditionCardValidOrInvalid = document.getElementById('finalConditionCardValidOrInvalid');
+let cardNumberWithMaskify = document.getElementById('cardNumberWithMaskify');
+let buttonValidateCardNumber = document.getElementById('buttonValidateCardNumber');
+let buttonaAdOthercard = document.getElementById('buttonaAdOthercard');
+let inputFinalProcess = document.getElementById('inputFinalProcess');
 
 
 inputExpirationDate.addEventListener('keyup', (e) => {
@@ -18,28 +18,38 @@ inputExpirationDate.addEventListener('keyup', (e) => {
     .replace(/\s/g, '/')
 });
 
-inputNumber.addEventListener('keyup', (e) => {
+inputCardNumber.addEventListener('keyup', (e) => {
   let prueba = e.target.value;
-  inputNumber.value = prueba.replace(/\D/g, '');
+  inputCardNumber.value = prueba.replace(/\D/g, '');
 });
 
 buttonValidateCardNumber.addEventListener('click', (event) => {
   event.preventDefault();
-  validator.isValid(inputNumber.value);
-  finalConditionCardValidOrInvalid.innerHTML = validator.isValid(inputNumber.value);
-  validator.maskify(inputNumber.value)
-  cardNumberWithMaskify.innerHTML = validator.maskify(inputNumber.value);
-  let numPos = document.querySelector('.numPos')
-  if (validator.isValid(inputNumber.value) === false) {
-    numPos.classList.add('br');
+  validator.isValid(inputCardNumber.value);
+  finalConditionCardValidOrInvalid.innerHTML = validator.isValid(inputCardNumber.value);
+  validator.maskify(inputCardNumber.value)
+  cardNumberWithMaskify.innerHTML = validator.maskify(inputCardNumber.value);
+  let divValidOrInvalidColor = document.querySelector('.divValidOrInvalidColor')
+  if (validator.isValid(inputCardNumber.value) === false) {
+    divValidOrInvalidColor.classList.add('br');
   } else {
-    numPos.classList.add('by');
+    divValidOrInvalidColor.classList.add('by');
   }
 
 });
 
-buttonaAdOthercard.addEventListener('click', () => {
 
+
+
+
+
+
+
+
+
+
+
+buttonaAdOthercard.addEventListener('click', () => {
   let inputs = document.getElementsByTagName("input");
   //console.log(inputs)
   for (var i = 0; i < inputs.length; i++) {
@@ -47,11 +57,11 @@ buttonaAdOthercard.addEventListener('click', () => {
   }
   finalConditionCardValidOrInvalid.innerHTML = "";
   cardNumberWithMaskify.innerHTML = "";
-  let numPos = document.querySelector('.numPos') /*retorna el primer elemento que coincide con el selector css dado como parámetro */
-  if (numPos.classList.remove('br')) {
-    numPos.classList.remove('br');
+  let divValidOrInvalidColor = document.querySelector('.divValidOrInvalidColor') /*retorna el primer elemento que coincide con el selector css dado como parámetro */
+  if (divValidOrInvalidColor.classList.remove('br')) {
+    divValidOrInvalidColor.classList.remove('br');
   } else {
-    numPos.classList.remove('by');
+    divValidOrInvalidColor.classList.remove('by');
   }
 })
 
@@ -63,13 +73,12 @@ inputFinalProcess.addEventListener('click', () => {
   }
   finalConditionCardValidOrInvalid.innerHTML = "";
   cardNumberWithMaskify.innerHTML = "";
-  let numPos = document.querySelector('.numPos')
-  if (numPos.classList.remove('br')) {
+  let divValidOrInvalidColor = document.querySelector('.divValidOrInvalidColor')
+  if (divValidOrInvalidColor.classList.remove('br')) {
     /*ACCEDERA LA LISTA DE CLASES DE UN ELEMENTO COMO UNA CADENA DE TEXTO DELIMITADA */
-    numPos.classList.remove('br');
+    divValidOrInvalidColor.classList.remove('br');
   } else {
-    numPos.classList.remove('by');
+    divValidOrInvalidColor.classList.remove('by');
   }
 })
-
 console.log(validator);
